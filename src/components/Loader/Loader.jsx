ef10e096919d689edbdd3492b15d0ff1c52943e9
PavelLesovoy <98907227+PavelLesovoy@users.noreply.github.com>
2022-12-02 15:41:19 +0200

    Loader fix

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
 import { ThreeDots } from 'react-loader-spinner';
+import css from './Loader.module.css';
 
 export const Loader = () => {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <ThreeDots height="160" width="160" />
+    <div className={css.Dot_loader}>
+      <ThreeDots
+        height="80"
+        width="80"
+        radius="9"
+        color="#4fa94d"
+        ariaLabel="three-dots-loading"
+        wrapperStyle={{}}
+        wrapperClassName=""
+        visible={true}
+      />
     </div>
   );
 };
